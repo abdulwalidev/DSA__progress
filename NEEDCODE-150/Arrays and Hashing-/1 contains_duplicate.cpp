2d8a692c1460adc0109cc08a2180d7557dbb1a7f
abdulwalidev <abdulwalidev@gmail.com>
2026-01-14 13:07:44 +0500

    Brief, clear description of what you changed

---
+++ NEEDCODE-150/Arrays and Hashing-/1 contains_duplicate.cpp
@@ -0,0 +1,24 @@
+
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        
+    
+    map<int, int> wali;
+
+    for(int num : nums){
+        wali[num]++;	}
+
+
+for(auto &p : wali)
+            {if(p.second>1)
+            {return true;
+            }}
+         return false;
+         
+         
+    
+  
+        //end
+    }
+};
