ad0145ef8ab1ecf96bb877a5e7a70e8a1a661566
abdulwalidev <abdulwalidev@gmail.com>
2026-01-16 22:29:51 +0500

    added problem 3 solution :two_sum

---
+++ NEEDCODE-150/Arrays and Hashing-/3. two_sum.cpp
@@ -0,0 +1,14 @@
+    class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> mapname;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int t = target - nums[i];
+            if (mapname.find(t) != mapname.end())
+                return {mapname[t], i};
+            else
+                mapname[nums[i]] = i;
+        }
+    }
+};
