acd98c69ca0f4e173b2075be5e4156aa8212177f
abdulwalidev <abdulwalidev@gmail.com>
2026-01-18 20:06:18 +0500

    did the encode decode problem no 6

--- NEEDCODE-150/Arrays and Hashing-/6. encode_decode_string.cpp
+++ NEEDCODE-150/Arrays and Hashing-/6. encode_decode_string.cpp
@@ -1,7 +1,43 @@
-#include<iostream>
-using namespace std;
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+
+
+	string result = "";
+	
+
+        for(string str : strs){
+result += to_string(str.length()) + "#" + str;
+            
+        }
+       
+        cout << result;
+	return result;
+}
+    
 
 
 
-
-int main(){}
+    vector<string> decode(string s) {
+vector<string> result;
+    int i = 0;
+    
+    while (i < s.length()) {
+        // Find '#'
+        int pos = s.find('#', i);
+        
+        // Get length
+        int len = stoi(s.substr(i, pos - i));
+        
+        // Get string
+        string str = s.substr(pos + 1, len);
+        result.push_back(str);
+        
+        // Move forward
+        i = pos + 1 + len;
+    }
+    
+    return result;
+    }
+};
