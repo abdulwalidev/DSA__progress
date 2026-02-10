4f6921ab8f873ffd22191835986340b60445a47c
abdulwalidev <abdulwalidev@gmail.com>
2026-02-10 20:51:56 +0500

    Update 2. valid_anagram.cpp

--- NEEDCODE-150/Arrays and Hashing-/2. valid_anagram.cpp
+++ NEEDCODE-150/Arrays and Hashing-/2. valid_anagram.cpp
@@ -5,7 +5,7 @@ public:
             sort(s.begin(), s.end());
             sort(t.begin(), t.end());
                 
-                if(s==t)
+                if(s==t)// main if
                 return true;//edge case
         }
         return false;
